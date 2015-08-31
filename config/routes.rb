ecf0c704611535edb02fefb5966ce29cad849c31
Mike Bradbury <wordsmithmb@gmail.com>
2015-08-31 13:54:31 -0700

    Customize bootstrap variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  root 'pages#home'
   get "about" => "pages#about" #creates about_path
   
   # The priority is based upon order of creation: first created -> highest priority.
